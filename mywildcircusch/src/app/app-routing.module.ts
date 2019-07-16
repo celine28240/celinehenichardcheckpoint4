df5d1e03a06e33022c96312f8a3743ecec77527b
celine28240 <celinehenichard@gmail.com>
2019-07-16 17:37:38 +0200

    create the home content

--- mywildcircusch/src/app/app-routing.module.ts
+++ mywildcircusch/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricesDatesComponent } from './prices-dates/prices-dates.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'artists',
+    component: ArtistsComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'prices',
+    component: PricesDatesComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- mywildcircusch/src/app/home/home.component.ts
+++ mywildcircusch/src/app/home/home.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  gallery: string[];
+
+  display: boolean = false;
+  modalsrc = '';
+
+  constructor() {
+
+
+    this.gallery = [
+     './assets/galleryPictures/images.jpeg',
+     './assets/galleryPictures/images2.jpeg',
+     './assets/galleryPictures/images3.jpeg',
+     './assets/galleryPictures/images4.jpeg',
+     './assets/galleryPictures/images5.jpeg',
+     './assets/galleryPictures/images6.jpeg',
+     './assets/galleryPictures/images7.jpeg',
+     './assets/galleryPictures/images8.jpeg',
+     './assets/galleryPictures/index.jpeg',
+    ];
+  }
+
+  onSelect(event: Event ): void {
+    const img: HTMLImageElement = event.target as HTMLImageElement;
+    this.modalsrc = img.src;
+    this.toggleModal();
+  }
+
+  toggleModal(): void {
+    const modalcontainer: HTMLElement = document.getElementById('myModal');
+    this.display = !this.display;
+ 
+    if ( this.display ) {
+      modalcontainer.style.display = 'block';
+    } else {modalcontainer.style.display = 'none'; }
+  }
 
   ngOnInit() {
   }
-
 }
