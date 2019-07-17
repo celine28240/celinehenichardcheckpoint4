72365c5b8ae4eac6fe578839fc17f0102f628e28
celine28240 <celinehenichard@gmail.com>
2019-07-17 15:18:16 +0200

    add backend file

---
+++ mywildcircusBack/src/main/java/com/mywildcircus/MywildcircusApplication.java
@@ -0,0 +1,13 @@
+package com.mywildcircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywildcircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywildcircusApplication.class, args);
+	}
+
+}
